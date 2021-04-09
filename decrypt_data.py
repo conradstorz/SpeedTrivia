60d0e4caa54f73cb0995412855d6f644bda298ff
Conrad Storz <conradstorz@gmail.com>
2021-04-09 12:47:02 -0400

    decyphers secrets w/user pword and sets env vars

--- decrypt_data.py
+++ decrypt_data.py
@@ -1,14 +1,27 @@
+"""Decrypt tokens and secrets with user supplied password.
+"""
+
 import os
 import sys
-from cryptocode import encrypt, decrypt
+from cryptocode import decrypt
 
-cyphertext = sys.argv[1]
-password = sys.argv[2]
+# Find these values at https://twilio.com/user/account
+# Encrypt with encrypt_data.py using a secure password and place result here.
+# Running decrypt_data.py will set environment variables to original value with user provided correct password.
+encrypted_account_sid = "vWFFuntfUAPQe/FTupv1OXV8AXWG5AaG8fzQp2ecunL7HA==*lmK4CGQhJTdlnZIJiVFmvQ==*DBCDTrmMz+oWPxgizpVkAA==*kaO0xIle1JPlQFS8syKmwQ=="
+encrypted_auth_token = "LJ0ctrM7RN1N7JS2tIm5hvfVX+F85RQmb8pXpyxYE+g=*Z5nguaX60YPaM7zs2r//Ew==*EMZDFrrPZfZsWVoehWZEug==*33yCyWvj08vvbYliZcAVuQ=="
 
-myDecryptedMessage = decrypt(cyphertext, password)
+if len(sys.argv) > 1:
+    password = sys.argv[1]
+else:
+    print('Password required on commandline.')
+    sys.exit(1)
+    
+ACCOUNT_SID = decrypt(encrypted_account_sid, password)
+AUTH_TOKEN = decrypt(encrypted_auth_token, password)
 
-print(myDecryptedMessage)
+os.system(f"setx ACCOUNT_SID {ACCOUNT_SID}")
+os.system(f"setx AUTH_TOKEN {AUTH_TOKEN}")
 
-cyphertext = encrypt(myDecryptedMessage, password)
-
-print(cyphertext)
+print('VScode must be re-started to access changed environment.')
+print('Program end.')
