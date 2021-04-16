dfe15fd873cf81478a39c1c2b931d5672f80bf81
Conrad Storz <conradstorz@gmail.com>
2021-04-15 23:04:36 -0400

    added form logic

--- Seleniumtest.py
+++ Seleniumtest.py
@@ -1,5 +1,7 @@
+import sys
 from time import sleep
 from selenium import webdriver
+from loguru import logger
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -8,27 +10,171 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
 
-web = webdriver.Chrome()
-web.get('https://form.jotform.com/202575177230149')
-TeamNameBoxID = 14
-GameRoundBoxID = 17
-GameQuestionBoxID = 18
-FirstHalfPointBoxID = 16
-SecondHalfPointBoxID = 19
-AnswerBoxID = 6
-SubmitButtonID = 7
-HalftimeAndFinalAnswerBoxID = [20, 21, 22, 23]
-FinalQuestionWagerID = 24
-Rounds = [1,2,3,'halftime',4,5,6,'final','tiebreaker']
-Questions = [3,3,3,1,3,3,3,1,1]
-Answers  =  [1,1,1,4,1,1,1,4,1]
-BasePoints = [6,6,6,0,9,9,9,20,0]
+TeamNameBoxID = 'input_14'
+GameRoundBoxID = 'input_17'
+GameQuestionBoxID = 'input_18'
+FirstHalfPointBoxID = 'input_16'
+SecondHalfPointBoxID = 'input_19'
+AnswerBoxID = 'input_6'
+SubmitButtonID = 'input_7'
+HalftimeAndFinalAnswerBoxIDs= [
+    'input_20',
+    'input_21', 
+    'input_22', 
+    'input_23',
+]
+FinalQuestionWagerID = 'input_24'
+Rounds = ['1','2','3','halftime','4','5','6','final','tiebreaker']
+Questions = [3,3,3,1,3,3,3,1,1]  # number of times an answer is required per round.
+Answers  =  [1,1,1,4,1,1,1,4,1]  # number of strings of answers per round.
+BasePoints = [
+    ['2', '4', '6',],
+    ['2', '4', '6',],   
+    ['2', '4', '6',],    
+    [],
+    ['5', '7', '9',],
+    ['5', '7', '9',],
+    ['5', '7', '9',],
+    ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+     '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',],  
+    [],     
+] 
+
+sample_answers = {
+    'first': {
+        'Round': '1',
+        'Question': '2',
+        'Points': '6',
+        'Answer': 'First round answer',
+    },
+    'halftime': {
+        'Round': 'Halftime',
+        'Answer': [
+            'alpha', 
+            'beta', 
+            'delta', 
+            'gamma',
+        ],
+        'Points': 'None. nothing is wagered'
+    },
+    'second': {
+        'Round': '5',
+        'Question': '2',
+        'Points': '9',
+        'Answer': 'second round answer',
+    },
+    'final': {
+        'Round0': 'Final',
+        'Answer': [
+            'alpha', 
+            'beta', 
+            'delta', 
+            'gamma',
+        ], 
+        'Points': '20',        
+    },
+    'tiebreaker': {
+        'Answer': 'The final decision (no points)'
+    }
+}
+
+firsthalf_field_ids = {
+    'Round': GameRoundBoxID,
+    'Question': GameQuestionBoxID,
+    'Points': FirstHalfPointBoxID,
+    'Answer': AnswerBoxID,
+}
+
+seconhalf_field_ids = {
+    'Round': GameRoundBoxID,
+    'Question': GameQuestionBoxID,
+    'Points': SecondHalfPointBoxID,
+    'Answer': AnswerBoxID,
+}
+
+halftime_field_ids = {
+    'Round': GameRoundBoxID,
+    'Answer': HalftimeAndFinalAnswerBoxIDs,
+}
+
+final_field_ids = {
+    'Round': GameRoundBoxID,
+    'Answer': HalftimeAndFinalAnswerBoxIDs,
+    'Points': FinalQuestionWagerID,
+}
+
+tiebreaker_field_ids = {
+    'Round': GameRoundBoxID,
+    'Answer': AnswerBoxID,
+}
+
+@logger.catch
+def main():
+
+    def Fill_a_field(value, field_id):
+        sleep(1)
+        team = web.find_element_by_id(field_id)
+        team.send_keys(value)
+        return
+
+    def Fill_a_dropdown(value, field_id):
+        sleep(1)
+        roundbox = Select(web.find_element_by_id(field_id))
+        roundbox.select_by_value(value)
+        return
+
+    Fill_field = {
+        'Round': Fill_a_dropdown,
+        'Question': Fill_a_dropdown,
+        'Points': Fill_a_dropdown,
+        'Answer': Fill_a_field,    
+    }
+
+    for k,v in sample_answers.items():
+        web = webdriver.Chrome()
+        web.get('https://form.jotform.com/202575177230149')        
+        Fill_a_field(f'Key: {k}', TeamNameBoxID)
+        if k == 'first':
+            for k1, v1 in v.items():
+                Fill_field[k1](v1, firsthalf_field_ids[k1])
+        if k == 'second':
+            for k1, v1 in v.items():
+                Fill_field[k1](v1, seconhalf_field_ids[k1])
+        if k == 'halftime':
+            for k1, v1 in v.items():
+                if k1 == 'Question': 
+                    pass  # there is no question number field here
+                if k1 == 'Points':
+                    pass  # truly do nothing there is no point wager.
+                if k1 == 'answer':
+                    pass  # must handle 4 answer boxes
+                else:
+                    Fill_field[k1](v1, halftime_field_ids[k1])            
+        if k == 'final':
+            for k1, v1 in v.items():
+                if k1 == 'Question': 
+                    pass  # there is no question number field here                
+                if k1 == 'answer':
+                    pass  # must handle 4 answer boxes
+                Fill_field[k1](v1, final_field_ids[k1])   
+        if k == 'tiebreaker':
+            for k1, v1 in v.items():
+                if k1 == 'Question': 
+                    pass  # there is no question number field here
+                if k1 == 'Points':
+                    pass  # truly do nothing there is no point wager.
+                Fill_field[k1](v1, tiebreaker_field_ids[k1])  
+        
+        sleep(5)
+        web.close()
+        sleep(1)
+    sys.exit(0)
 
 
-sleep(1)
-teamname = 'Alice'
-team = web.find_element_by_xpath('//*[@id="input_14"]')
-team.send_keys(teamname)
+"""
+Fill_a_field(f'Key: {k}', AnswerBoxID)
+
+
 
 sleep(1)
 round = '1'
@@ -49,3 +195,10 @@ sleep(1)
 answer = 'Wonderland'
 answerbox = web.find_element_by_xpath('//*[@id="input_6"]')
 answerbox.send_keys(answer)
+
+sleep(10)
+web.close()
+"""
+
+
+main()
