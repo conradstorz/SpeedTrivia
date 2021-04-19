fac4f2befab8c789f2bb06d501f591cb1a7f3333
Conrad Storz <conradstorz@gmail.com>
2021-04-18 20:32:47 -0400

    reformated and tested

--- Seleniumtest.py
+++ Seleniumtest.py
@@ -9,201 +9,231 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
-
-TeamNameBoxID = 'input_14'
-GameRoundBoxID = 'input_17'
-GameQuestionBoxID = 'input_18'
-FirstHalfPointBoxID = 'input_16'
-SecondHalfPointBoxID = 'input_19'
-AnswerBoxID = 'input_6'
-SubmitButtonID = 'input_7'
-HalftimeAndFinalAnswerBoxIDs= [
-    'input_20',
-    'input_21', 
-    'input_22', 
-    'input_23',
+TeamNameBoxID = "input_14"
+GameRoundBoxID = "input_17"
+GameQuestionBoxID = "input_18"
+FirstHalfPointBoxID = "input_16"
+SecondHalfPointBoxID = "input_19"
+AnswerBoxID = "input_6"
+SubmitButtonID = "input_7"
+HalftimeAndFinalAnswerBoxIDs = [
+    "input_20",
+    "input_21",
+    "input_22",
+    "input_23",
 ]
-FinalQuestionWagerID = 'input_24'
-Rounds = ['1','2','3','halftime','4','5','6','final','tiebreaker']
-Questions = [3,3,3,1,3,3,3,1,1]  # number of times an answer is required per round.
-Answers  =  [1,1,1,4,1,1,1,4,1]  # number of strings of answers per round.
-BasePoints = [
-    ['2', '4', '6',],
-    ['2', '4', '6',],   
-    ['2', '4', '6',],    
-    [],
-    ['5', '7', '9',],
-    ['5', '7', '9',],
-    ['5', '7', '9',],
-    ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
-     '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',],  
-    [],     
-] 
+FinalQuestionWagerID = "input_24"
 
+
+def Fill_a_field(webobj, value, field_id):
+    logger.debug("fill field")
+    logger.debug(f"field: {field_id} Value: {value}")
+    sleep(0.1)
+    field = webobj.find_element_by_id(field_id)
+    field.send_keys(value)
+    return
+
+
+def Fill_a_dropdown(webobj, value, field_id):
+    logger.debug("fill dropdown")
+    logger.debug(f"field: {field_id} Value: {value}")
+    sleep(0.1)
+    dropdn = Select(webobj.find_element_by_id(field_id))
+    dropdn.select_by_value(value)
+    return
+
+
+def Fill_four_answers(webobj, values, field_ids):
+    """This requires multiple values and fields in lists"""
+    logger.debug("fill 4 answers")
+    logger.debug(f"field: {field_ids} Value: {values}")
+    for indx, field in enumerate(field_ids):
+        Fill_a_field(webobj, values[indx], field)
+    return
+
+
+def Click_a_button(webobj, value, field_id):
+    sleep(0.1)
+    logger.debug("click option")
+    logger.debug(f"field: {field_id} Value: {value}")
+    button = webobj.find_element_by_id(field_id)
+    if value == True:
+        button.click()
+    else:
+        logger.debug("Submit button not pressed by request.")
+    return
+
+
+def Pass_(value, field_id):
+    logger.debug("pass option")
+    logger.debug(f"field: {field_id} Value: {value}")
+    pass
+
+
+WEBFORM = {
+    "1": {
+        "team": {"fld": TeamNameBoxID, "func": Fill_a_field},
+        "round": {"fld": GameRoundBoxID, "func": Fill_a_dropdown},
+        "question": {"fld": GameQuestionBoxID, "func": Fill_a_dropdown},
+        "points": {"fld": FirstHalfPointBoxID, "func": Fill_a_dropdown},
+        "answer": {"fld": AnswerBoxID, "func": Fill_a_field},
+        "submit": {"fld": SubmitButtonID, "func": Click_a_button},
+    },
+    "2": {
+        "team": {"fld": TeamNameBoxID, "func": Fill_a_field},
+        "round": {"fld": GameRoundBoxID, "func": Fill_a_dropdown},
+        "question": {"fld": GameQuestionBoxID, "func": Fill_a_dropdown},
+        "points": {"fld": FirstHalfPointBoxID, "func": Fill_a_dropdown},
+        "answer": {"fld": AnswerBoxID, "func": Fill_a_field},
+        "submit": {"fld": SubmitButtonID, "func": Click_a_button},
+    },
+    "3": {
+        "team": {"fld": TeamNameBoxID, "func": Fill_a_field},
+        "round": {"fld": GameRoundBoxID, "func": Fill_a_dropdown},
+        "question": {"fld": GameQuestionBoxID, "func": Fill_a_dropdown},
+        "points": {"fld": FirstHalfPointBoxID, "func": Fill_a_dropdown},
+        "answer": {"fld": AnswerBoxID, "func": Fill_a_field},
+        "submit": {"fld": SubmitButtonID, "func": Click_a_button},
+    },
+    "Halftime": {
+        "team": {"fld": TeamNameBoxID, "func": Fill_a_field},
+        "round": {"fld": GameRoundBoxID, "func": Fill_a_dropdown},
+        "question": {"fld": GameQuestionBoxID, "func": Fill_a_dropdown},
+        "points": {"fld": FirstHalfPointBoxID, "func": Fill_a_dropdown},
+        "answer": {"fld": HalftimeAndFinalAnswerBoxIDs, "func": Fill_four_answers},
+        "submit": {"fld": SubmitButtonID, "func": Click_a_button},
+    },
+    "4": {
+        "team": {"fld": TeamNameBoxID, "func": Fill_a_field},
+        "round": {"fld": GameRoundBoxID, "func": Fill_a_dropdown},
+        "question": {"fld": GameQuestionBoxID, "func": Fill_a_dropdown},
+        "points": {"fld": SecondHalfPointBoxID, "func": Fill_a_dropdown},
+        "answer": {"fld": AnswerBoxID, "func": Fill_a_field},
+        "submit": {"fld": SubmitButtonID, "func": Click_a_button},
+    },
+    "5": {
+        "team": {"fld": TeamNameBoxID, "func": Fill_a_field},
+        "round": {"fld": GameRoundBoxID, "func": Fill_a_dropdown},
+        "question": {"fld": GameQuestionBoxID, "func": Fill_a_dropdown},
+        "points": {"fld": SecondHalfPointBoxID, "func": Fill_a_dropdown},
+        "answer": {"fld": AnswerBoxID, "func": Fill_a_field},
+        "submit": {"fld": SubmitButtonID, "func": Click_a_button},
+    },
+    "6": {
+        "team": {"fld": TeamNameBoxID, "func": Fill_a_field},
+        "round": {"fld": GameRoundBoxID, "func": Fill_a_dropdown},
+        "question": {"fld": GameQuestionBoxID, "func": Fill_a_dropdown},
+        "points": {"fld": SecondHalfPointBoxID, "func": Fill_a_dropdown},
+        "answer": {"fld": AnswerBoxID, "func": Fill_a_field},
+        "submit": {"fld": SubmitButtonID, "func": Click_a_button},
+    },
+    "Final": {
+        "team": {"fld": TeamNameBoxID, "func": Fill_a_field},
+        "round": {"fld": GameRoundBoxID, "func": Fill_a_dropdown},
+        "question": {"fld": GameQuestionBoxID, "func": Fill_a_dropdown},
+        "points": {"fld": FinalQuestionWagerID, "func": Fill_a_field},
+        "answer": {"fld": HalftimeAndFinalAnswerBoxIDs, "func": Fill_four_answers},
+        "submit": {"fld": SubmitButtonID, "func": Click_a_button},
+    },
+    "Tiebreaker": {
+        "team": {"fld": TeamNameBoxID, "func": Fill_a_field},
+        "round": {"fld": GameRoundBoxID, "func": Fill_a_dropdown},
+        "question": {"fld": GameQuestionBoxID, "func": Fill_a_dropdown},
+        "points": {"fld": FirstHalfPointBoxID, "func": Fill_a_dropdown},
+        "answer": {"fld": AnswerBoxID, "func": Fill_a_field},
+        "submit": {"fld": SubmitButtonID, "func": Click_a_button},
+    },
+}
+
+SENDFORM = False
 sample_answers = {
-    'first': {
-        'Round': '1',
-        'Question': '2',
-        'Points': '6',
-        'Answer': 'First round answer',
+    "1": {
+        "team": "FirstRound",
+        "round": "1",
+        "question": "2",
+        "points": "6",
+        "answer": "First round answer",
+        "submit": SENDFORM,
     },
-    'halftime': {
-        'Round': 'Halftime',
-        'Answer': [
-            'alpha', 
-            'beta', 
-            'delta', 
-            'gamma',
+    "halftime": {
+        "team": "Halftime Round",
+        "round": "Halftime",
+        "answer": [
+            "alpha",
+            "beta",
+            "delta",
+            "gamma",
         ],
-        'Points': 'None. nothing is wagered'
+        "submit": SENDFORM,
     },
-    'second': {
-        'Round': '5',
-        'Question': '2',
-        'Points': '9',
-        'Answer': 'second round answer',
+    "4": {
+        "team": "Second Round",
+        "round": "4",
+        "question": "2",
+        "points": "9",
+        "answer": "second round answer",
+        "submit": SENDFORM,
     },
-    'final': {
-        'Round': 'Final',
-        'Answer': [
-            'alpha', 
-            'beta', 
-            'delta', 
-            'gamma',
-        ], 
-        'Points': '20',        
+    "final": {
+        "team": "Final Questions",
+        "round": "Final",
+        "answer": [
+            "alpha",
+            "beta",
+            "delta",
+            "gamma",
+        ],
+        "points": "20",
+        "submit": SENDFORM,
     },
-    'tiebreaker': {
-        'Round': 'Tiebreaker',
-        'Answer': 'The final decision (no points)'
-    }
+    "tiebreaker": {
+        "team": "Tiebreaker round",
+        "round": "Tiebreaker",
+        "answer": "The final decision (no points)",
+        "submit": SENDFORM,
+    },
 }
 
-firsthalf_field_ids = {
-    'Round': GameRoundBoxID,
-    'Question': GameQuestionBoxID,
-    'Points': FirstHalfPointBoxID,
-    'Answer': AnswerBoxID,
-}
 
-seconhalf_field_ids = {
-    'Round': GameRoundBoxID,
-    'Question': GameQuestionBoxID,
-    'Points': SecondHalfPointBoxID,
-    'Answer': AnswerBoxID,
-}
+def Fill_and_submit_trivia_form(data):
+    """Launch a browser and fill fields of webform then submit.
+    The format of the 'data' variable varies based on the form to be filled.
+    Round 1 and Round 2 forms are identical differing only in the 'field labels' in use.
+    Halftime and FinalRound are the same but use 4 answer boxes. These rounds require
+    the 'answer' and 'field' variable to be a list of 4 items each corresponding to
+    the box 'field label' and 'answer text' for each of the four answers.
+    Tiebrasker round is different in that it only has an answer and no points field.
+    """
+    web = webdriver.Chrome()
+    web.get("https://form.jotform.com/202575177230149")
+    fields_and_functions = WEBFORM[data["round"]]
+    logger.debug(fields_and_functions)
+    logger.debug("...")
+    logger.debug(data)
+    # fill the 'Round' value on the form first.
+    Round = data.pop("round")
+    fields_and_functions["round"]["func"](
+        web, Round, fields_and_functions["round"]["fld"]
+    )
+    # form is now initialized to accept the correct data
+    for field, value in data.items():
+        logger.debug(f"Field: {field} Value: {value}")
+        if field in fields_and_functions.keys():
+            fields_and_functions[field]["func"](
+                web, value, fields_and_functions[field]["fld"]
+            )
+    sleep(5)
+    web.close()
+    return
 
-halftime_field_ids = {
-    'Round': GameRoundBoxID,
-    'Answer': HalftimeAndFinalAnswerBoxIDs,
-}
-
-final_field_ids = {
-    'Round': GameRoundBoxID,
-    'Answer': HalftimeAndFinalAnswerBoxIDs,
-    'Points': FinalQuestionWagerID,  # TODO edge case. this field is an input box. the other rounds this is a dropdown.
-}
-
-tiebreaker_field_ids = {
-    'Round': GameRoundBoxID,
-    'Answer': AnswerBoxID,
-}
 
 @logger.catch
 def main():
-
-    def Fill_a_field(value, field_id):
-        sleep(.1)
-        team = web.find_element_by_id(field_id)
-        team.send_keys(value)
-        return
-
-    def Fill_a_dropdown(value, field_id):
-        sleep(.1)
-        roundbox = Select(web.find_element_by_id(field_id))
-        roundbox.select_by_value(value)
-        return
-
-    Fill_field = {
-        'Round': Fill_a_dropdown,
-        'Question': Fill_a_dropdown,
-        'Points': Fill_a_dropdown,
-        'Answer': Fill_a_field,    
-    }
-
-    for k,v in sample_answers.items():
-        web = webdriver.Chrome()
-        web.get('https://form.jotform.com/202575177230149')        
-        Fill_a_field(f'Key: {k}', TeamNameBoxID)
-        if k == 'first':
-            for k1, v1 in v.items():
-                Fill_field[k1](v1, firsthalf_field_ids[k1])
-        if k == 'second':
-            for k1, v1 in v.items():
-                Fill_field[k1](v1, seconhalf_field_ids[k1])
-        if k == 'halftime':
-            for k1, v1 in v.items():
-                if k1 == 'Question': 
-                    pass  # there is no question number field here
-                elif k1 == 'Points':
-                    pass  # truly do nothing there is no point wager.
-                elif k1 == 'Answer':
-                    pass  # must handle 4 answer boxes
-                else:
-                    Fill_field[k1](v1, halftime_field_ids[k1])            
-        if k == 'final':
-            for k1, v1 in v.items():
-                if k1 == 'Question': 
-                    pass  # there is no question number field here                
-                elif k1 == 'Answer':
-                    pass  # must handle 4 answer boxes
-                elif k1 == 'Points':  # EDGE-CASE. This round uses input not dropdown.
-                    Fill_a_field(v1, final_field_ids[k1])
-                else:
-                    Fill_field[k1](v1, final_field_ids[k1])   
-        if k == 'tiebreaker':
-            for k1, v1 in v.items():
-                if k1 == 'Question': 
-                    pass  # there is no question number field here
-                elif k1 == 'Points':
-                    pass  # truly do nothing there is no point wager.
-                else:
-                    Fill_field[k1](v1, tiebreaker_field_ids[k1])  
-        
-        sleep(5)
-        web.close()
-        sleep(1)
+    for k, sampleform in sample_answers.items():
+        Fill_and_submit_trivia_form(sample_answers[k])
+    sleep(1)
     sys.exit(0)
 
 
-"""
-Fill_a_field(f'Key: {k}', AnswerBoxID)
-
-
-
-sleep(1)
-round = '1'
-roundbox = Select(web.find_element_by_id('input_17'))
-roundbox.select_by_value(round)
-
-sleep(1)
-questionnumber = '3'
-questionnumberbox = Select(web.find_element_by_id('input_18'))
-questionnumberbox.select_by_value(questionnumber)
-
-sleep(1)
-pointvalue = '6'
-pointvaluebox = Select(web.find_element_by_id('input_16'))
-pointvaluebox.select_by_value(pointvalue)
-
-sleep(1)
-answer = 'Wonderland'
-answerbox = web.find_element_by_xpath('//*[@id="input_6"]')
-answerbox.send_keys(answer)
-
-sleep(10)
-web.close()
-"""
-
-
-main()
+if __name__ == "__main__":
+    main()
