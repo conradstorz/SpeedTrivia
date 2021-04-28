85bc4401438eedb22a29b38b6be06b6315915f7a
Douglas Storz <douglasstorz@gmail.com>
2021-04-28 14:31:43 -0400

    Added Team object and tests

---
+++ team.py
@@ -0,0 +1,64 @@
+"""
+Team object?
+Team
+    members
+    current round
+        used points
+        current question
+"""
+import datetime
+
+class Team():
+
+    def __init__(self, teamname):
+        self.teamname = teamname
+        self.round = 1
+        self.question = 1
+        self.spent_points = []
+
+    def submit_answer(self, points):
+        if points in self.spent_points:
+            return False
+
+        valid = False
+        if self.round in (1,2,3) and points in (2,4,6):
+            valid = True
+        elif self.round in (4,5,6) and points in (5,7,9):
+            valid = True
+        elif self.round == 'halftime':
+            valid = True
+        elif self.round == 'final' and points >= 0 and points <= 20:
+            valid = True
+        elif self.round == 'tiebreaker':
+            raise NotImplementedError("Don't know what points are valid for tiebreaker. None?")
+        
+        if valid:
+            self.spent_points.append(points)
+            self.increment_question()
+            return True
+        else:
+            return False
+
+    def increment_question(self):
+        # increment rounds
+        if self.round in ('halftime', 'final') or self.question == 3:
+            self.increment_round()
+        else:
+            self.question += 1
+
+    def increment_round(self):
+        self.question = 1
+        self.spent_points = []
+
+        if self.round == 3:
+            self.round = 'halftime'
+        elif self.round == 'halftime':
+            self.round = 4
+        elif self.round == 6:
+            self.round = 'final'
+        elif self.round == 'final':
+            self.round = 'tiebreaker'
+        elif self.round == 'tiebreaker':
+            raise ValueError("Tiebreaker is the final round!")
+        else:
+            self.round += 1
---
+++ test_team.py
@@ -0,0 +1,83 @@
+from team import Team
+
+def test_creating_a_team_sets_defaults():
+    team = Team("Test Team")
+
+    assert team.teamname == "Test Team"
+    assert team.round == 1
+    assert team.question == 1
+
+def test_team_tracks_the_current_question_and_available_points():
+    team = Team("Test Team")
+
+    resp = team.submit_answer(4)
+
+    assert resp == True
+    assert team.round == 1
+    assert team.question == 2
+    assert team.spent_points == [4]
+
+def test_team_returns_False_if_point_value_was_already_used_this_round():
+    team = Team("Test Team")
+
+    team.submit_answer(4)
+    resp = team.submit_answer(4)
+
+    assert resp == False
+
+def test_team_transitions_rounds_automatically():
+    team = Team("Test Team")
+
+    team.submit_answer(2)
+    team.submit_answer(4)
+    team.submit_answer(6)
+
+    assert team.round == 2
+    assert team.question == 1
+
+def test_team_transitions_to_halftime():
+    team = Team("Test Team")
+    team.round = 3
+    team.question = 3
+
+    team.submit_answer(4)
+
+    assert team.round == 'halftime'
+    assert team.question == 1
+
+def test_team_transitions_to_final():
+    team = Team("Test Team")
+    team.round = 6
+    team.question = 3
+    team.submit_answer(9)
+
+    assert team.round == 'final'
+    assert team.question == 1
+
+def test_team_transitions_to_tiebreaker():
+    team = Team("Test Team")
+    team.round = 'final'
+    team.submit_answer(20)
+
+    assert team.round == 'tiebreaker'
+    assert team.question == 1
+
+def test_team_validates_points_based_on_round():
+    team = Team("Test Team")
+
+    resp = team.submit_answer(5)
+    assert resp == False
+    resp = team.submit_answer(4)
+    assert resp == True
+
+    team.round = 4
+    resp = team.submit_answer(2)
+    assert resp == False
+    resp = team.submit_answer(7)
+    assert resp == True
+
+    team.round = 'final'
+    resp = team.submit_answer(21)
+    assert resp == False
+    resp = team.submit_answer(20)
+    assert resp == True
